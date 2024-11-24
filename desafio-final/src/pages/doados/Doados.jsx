f298e82a366d28aa0216671e055947f83df65d1e
J-Pedr0 <jpnolasco70@gmail.com>
2024-11-24 19:15:44 -0300

    Criação e estilização da page doados

--- desafio-final/src/pages/doados/Doados.jsx
+++ desafio-final/src/pages/doados/Doados.jsx
@@ -1,9 +1,33 @@
 import scss from "./Doados.module.scss";
 
+import livroProtagonista from "../../assets/livro_protagonista.png";
+
 export default function Doados() {
+  const livros = [
+    {
+      id: 1,
+      imagem: livroProtagonista,
+      titulo: "O Protagonista",
+      autor: "Susanne Andrade",
+      genero: "Ficção",
+    },
+  ];
+
   return (
     <main>
-      <h1>Eu sou o Doados</h1>
+      <section className={scss.boxTitulo}>
+        <h1>Livros Doados</h1>
+      </section>
+      <section className={scss.boxLivros}>
+        {livros.map((livro) => (
+          <div key={livro.id} className={scss.boxCard}>
+            <img src={livro.imagem} alt={`Capa do livro ${livro.titulo}`} />
+            <p>{livro.titulo}</p>
+            <p>{livro.autor}</p>
+            <p>{livro.genero}</p>
+          </div>
+        ))}
+      </section>
     </main>
   );
 }

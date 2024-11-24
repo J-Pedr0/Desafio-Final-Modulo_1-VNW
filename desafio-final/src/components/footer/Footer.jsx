acff9c04e92cc455e9758a5a13d7dc7c95fb4853
J-Pedr0 <jpnolasco70@gmail.com>
2024-11-24 16:53:31 -0300

    Estilização da footer

--- desafio-final/src/components/footer/Footer.jsx
+++ desafio-final/src/components/footer/Footer.jsx
@@ -1,9 +1,29 @@
 import scss from "./Footer.module.scss";
 
+import facebook from "../../assets/facebook.png";
+import twitter from "../../assets/twitter.png";
+import youtube from "../../assets/youtube.png";
+import linkedin from "../../assets/linkedin.png";
+import instagram from "../../assets/instagram.png";
+
 export default function Footer() {
   return (
     <footer>
-      <h1>Eu sou o footer</h1>
+      <section className={scss.boxContato}>
+        <div className={scss.boxDivContato}>
+          <p>4002-8922</p>
+        </div>
+        <div className={scss.boxSocial}>
+          <img src={facebook} alt="ícone facebook" />
+          <img src={twitter} alt="ícone twitter" />
+          <img src={youtube} alt="ícone youtube" />
+          <img src={linkedin} alt="ícone linkedin" />
+          <img src={instagram} alt="ícone instagram" />
+        </div>
+      </section>
+      <section>
+        <p>Layout desenvolvido pela Vai Na Web para fins educativos - 2024 </p>
+      </section>
     </footer>
   );
 }
